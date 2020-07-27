1205b91e966f6c539b4e0e330280a0156ce00e02
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-07-27 13:08:49 +0200

    add navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BooksModule],
+  imports: [BrowserModule, BooksModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/layout/navigation/navigation.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ws-navigation',
+  templateUrl: './navigation.component.pug',
+  styleUrls: ['./navigation.component.sass'],
+})
+export class NavigationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
