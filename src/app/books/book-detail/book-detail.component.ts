b43cc8649c8693f713133c0981b4a5223de4b23c
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-07-29 10:09:57 +0200

    add simple unrecommended way of loading a single book

--- src/app/books/book-detail/book-detail.component.ts
+++ src/app/books/book-detail/book-detail.component.ts
@@ -11,9 +11,18 @@ import { BookApi } from '../book-api.service';
   styleUrls: ['./book-detail.component.sass'],
 })
 export class BookDetailComponent {
+  bookScalar: Book;
   book: Observable<Book>;
 
   constructor(private route: ActivatedRoute, private bookApi: BookApi) {
+    // Simple (not recommended), should be placed in ngOnInit
+    this.route.params.subscribe(params =>
+      this.bookApi
+        .getBookByIsbn(params.isbn)
+        .subscribe(bookFromApi => (this.bookScalar = bookFromApi))
+    );
+
+    // Advanced
     this.book = this.route.params.pipe(
       map(params => params.isbn),
       switchMap(isbn => this.bookApi.getBookByIsbn(isbn))
